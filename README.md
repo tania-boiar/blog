2fd7aa0a09a6acbae74eec02d08adc21366e1cd1
Tetiana.Boiar <taniavasylchyk@gmail.com>
2023-07-28 19:34:47 +0300

    react router fixes & added README

---
+++ README.md
@@ -0,0 +1,27 @@
+# Blog
+[Demo](https://tania-boiar.github.io/blog/)
+
+## Technologies Used
+
+This project uses React, TypeScript, HTML, Material UI ...
+
+## Getting Started with Create React App
+
+This project was bootstrapped with [Vite](https://vitejs.dev/).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm run dev`
+
+Runs the app in the development mode.\
+Open [http://localhost:5173/](http://localhost:5173/) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm run build`
+
+Builds the app for production to the `dist` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -8,6 +8,7 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { NavLink } from 'react-router-dom';
 
 const defaultTheme = createTheme();
 
@@ -120,10 +121,10 @@ export const SignInPage: React.FC = () => {
             <Grid container>
                 <Grid item>
                 {"Don't have an account? "}
-                  <Link href="/sign-up" variant="body1">
-                    {"Sign Up"}
-                  </Link>
                 </Grid>
+                <NavLink to="/sign-up">
+                  Sign Up
+                </NavLink>
               </Grid>
           </Box>
         </Grid>
