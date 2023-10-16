e8ac2a5ef104215dc3648b7f04d363101e42ed0b
Tetiana.Boiar <taniavasylchyk@gmail.com>
2023-10-16 21:44:34 +0300

    terms basic layout

--- src/pages/TermsPage.tsx
+++ src/pages/TermsPage.tsx
@@ -1,5 +1,88 @@
-export const TermsPage: React.FC = () => (
-  <div>
-    <h1>Terms Page</h1>
-  </div>
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { Button, Container } from '@mui/material';
+
+
+
+export const TermsPage: React.FC= () => (
+  <React.Fragment>
+  <CssBaseline />
+    <AppBar position="fixed" elevation={0}>
+      <Toolbar sx={{ backgroundColor: "#040D32", height: "154px" }} >
+        <Container maxWidth="lg">
+          <Box display={'flex'} flexDirection={'column'}>
+            <a href="#">
+              <img src="./assets/images/logo-white.svg" />
+            </a>
+            <Button sx={{ color: "#fff", textTransform: "none", justifyContent: "flex-start"}}>
+            <ChevronLeftIcon fontSize='small' sx={{marginRight: "14px"}} />
+            <Typography variant="h5" component="div" sx={{ fontWeight: "700"}}>
+              Product legal information
+            </Typography>
+            </Button>
+          </Box>
+        </Container>
+      </Toolbar>
+    </AppBar>
+  <Toolbar />
+
+  <main>
+    <Box
+      sx={{
+        bgcolor: 'background.paper',
+        pt: 20,
+        pb: 10,
+        color: "#1A1E2C",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Typography
+          component="h1"
+          variant="h4"
+          pb="40px"
+          fontWeight={700}
+        >
+          Privacy Policy
+        </Typography>
+        <Typography mb="22px" variant="h6" component="h2" fontWeight={700}>
+          1. The standard Lorem Ipsum passage, used since the 1500s
+        </Typography>
+        <Typography paragraph mb="22px">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Typography>
+
+        <Typography paragraph mb="22px">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Typography>
+
+        <Typography paragraph mb="22px">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Typography>
+
+        <Typography variant="h6" mb="22px" component="h2" fontWeight={700}>
+          2. The standard Lorem Ipsum passage
+        </Typography>
+        <Typography paragraph mb="22px">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Typography>
+
+        <Typography paragraph mb="22px">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Typography>
+
+        <Typography paragraph mb="22px">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Typography>
+
+        <Button variant="outlined" sx={{borderRadius: "3px", borderColor: "#6F7C8B80", color: "#6F7C8B", width: "108px"}}>Back</Button>
+      </Container>
+    </Box>
+  </main>
+
+</React.Fragment>
 );
